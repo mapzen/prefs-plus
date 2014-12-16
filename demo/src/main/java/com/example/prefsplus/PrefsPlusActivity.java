103ca51b6c92542b38ac517ec2d638175d55f06e
Chuck Greb <charles.greb@gmail.com>
2014-12-16 14:36:34 -0500

    Demo app cleanup

--- demo/src/main/java/com/example/prefsplus/PrefsPlusActivity.java
+++ demo/src/main/java/com/example/prefsplus/PrefsPlusActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PrefsPlusActivity extends Activity {
     private PrefsPlusFragment fragment;
@@ -48,10 +52,42 @@ public class PrefsPlusActivity extends Activity {
     public static class PrefsPlusFragment extends Fragment {
         public static final String TAG = PrefsPlusFragment.class.getSimpleName();
 
+        private TextView integerTextView;
+        private TextView floatTextView;
+        private TextView integerListTextView;
+
+        private int intValue;
+        private float floatValue;
+        private int intListValue;
+
+        private SharedPreferences prefs;
+        private Resources res;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_lost, container, false);
+            final View view = inflater.inflate(R.layout.fragment_prefs_plus, container, false);
+            integerTextView = (TextView) view.findViewById(R.id.integer_value);
+            floatTextView = (TextView) view.findViewById(R.id.float_value);
+            integerListTextView = (TextView) view.findViewById(R.id.integer_list_value);
+            prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            res = getResources();
+            return view;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            intValue = prefs.getInt(res.getString(R.string.integer_key),
+                    res.getInteger(R.integer.integer_default_value));
+            floatValue = prefs.getFloat(res.getString(R.string.float_key),
+                    res.getInteger(R.integer.integer_default_value));
+            intListValue = prefs.getInt(res.getString(R.string.integer_list_key),
+                    res.getInteger(R.integer.integer_default_value));
+
+            integerTextView.setText(Integer.toString(intValue));
+            floatTextView.setText(Float.toString(floatValue));
+            integerListTextView.setText(Integer.toString(intListValue));
         }
     }
 }
